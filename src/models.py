de6f43948b72e93f72365359f6dc30c7114d5137
yanachernova <yana.chernova94@gmail.com>
2020-01-14 19:52:43 +0000

    second commit

--- src/models.py
+++ src/models.py
@@ -9,54 +9,59 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    lastname = Column(String(250), nullable=True)
-    email = Column(String(250), nullable=False)
+    username = Column(String(30), nullable=False)
     password = Column(String(250), nullable=False)
-    edad = Column(String(250), nullable=True)
-class History(Base):
-    __tablename__ = 'histories'
-    id = Column(Integer, primary_key=True)
-    photo = Column(String(250), nullable=True)
-    video = Column(String(250), nullable=True)
-    text = Column(String(250), nullable=True)
-    sticker = Column(String(250), nullable=False)
-    form = Column(String(250), nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    history = relationship(User)
+    fullname = Column(String(250), nullable=True)
+    email = Column(String(250), nullable=False)
+    phone_num = Column(Integer, nullable=False)
+    profile_pic_url = Column(String(250), nullable=False)
+
 class Follower(Base):
-    __tablename__ = 'Followers'
-    user_id = Column(Integer, ForeignKey('users.id'),primary_key=True)
+    __tablename__ = 'followers'
+    timestamp = Column(String(250), nullable=False)  
+    denayorrejectoption = Column(String(250), nullable=True) 
+    follower_status = Column(String(250), nullable=False)  
     follower_id = Column(Integer, ForeignKey('users.id'),primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'),primary_key=True)
     follower = relationship(User)
-class Profilephoto(Base):
-    __tablename__ = 'profilephotos'
-    id = Column(Integer, primary_key=True)
-    width = Column(String(250), nullable=False)
-    heigth = Column(String(250), nullable=False)
-    photo = Column(String(250), nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    profilephoto = relationship(User)
+    user = relationship(User)
+
+
 class Post(Base):
     __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
-    photo = Column(String(250), nullable=False)
-    video = Column(String(250), nullable=False)
-    text = Column(String(250), nullable=False)
-    tag = Column(String(250), nullable=False)
-    address = Column(String(250), nullable=False)
-    hashtag = Column(String(250), nullable=False)
+    photo_id = Column(String(250), nullable=False)
+    img_url = Column(String(250), nullable=False)
+    timestamp = Column(String(250), nullable=False)
+    pic_descrition = Column(String(250), nullable=True)
+    location = Column(String(250), nullable=True)
     user_id = Column(Integer, ForeignKey('users.id'))
-    post = relationship(User)
+    user = relationship(User)
+
 class Comment(Base):
     __tablename__ = 'comments'
     id = Column(Integer, primary_key=True)
-    text = Column(String(250), nullable=False)
-    symbol = Column(String(250), nullable=True)
-    reference = Column(String(250), nullable=True)
+    photo_id = Column(Integer, nullable=False)
+    comment = Column(String(250), nullable=True)
+    img_url = Column(String(250), nullable=False)
+    timestamp = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
-    post_id = Column(Integer, ForeignKey('users.id'))
-    comment = relationship(Post)
+    user = relationship(User)
+    posts_id = Column(Integer, ForeignKey('posts.id'))
+    posts = relationship(Post)
+
+class Like(Base):
+    __tablename__ = 'likes'
+    id = Column(Integer, primary_key=True)
+    photo_id = Column(Integer, nullable=False)
+    liker_id = Column(Integer, nullable=False)
+    timestamp = Column(String(250), nullable=False)
+    img_url = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(User)
+    posts_id = Column(Integer, ForeignKey('posts.id'))
+    posts = relationship(Post)
+
     def to_dict(self):
         return {}
 ## Draw from SQLAlchemy base
