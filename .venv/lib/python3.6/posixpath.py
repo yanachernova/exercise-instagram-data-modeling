061b8621d0d67434b8086e2772da95df6d4c3416
yanachernova <yana.chernova94@gmail.com>
2020-01-14 19:24:54 +0000

    first commit

---
+++ .venv/lib/python3.6/posixpath.py
@@ -0,0 +1,1 @@
+/home/gitpod/.pyenv/versions/3.6.10/lib/python3.6/posixpath.py
--- src/models.py
+++ src/models.py
@@ -5,29 +5,59 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
-
 Base = declarative_base()
-
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    lastname = Column(String(250), nullable=True)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    edad = Column(String(250), nullable=True)
+class History(Base):
+    __tablename__ = 'histories'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
+    photo = Column(String(250), nullable=True)
+    video = Column(String(250), nullable=True)
+    text = Column(String(250), nullable=True)
+    sticker = Column(String(250), nullable=False)
+    form = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    history = relationship(User)
+class Follower(Base):
+    __tablename__ = 'Followers'
+    user_id = Column(Integer, ForeignKey('users.id'),primary_key=True)
+    follower_id = Column(Integer, ForeignKey('users.id'),primary_key=True)
+    follower = relationship(User)
+class Profilephoto(Base):
+    __tablename__ = 'profilephotos'
+    id = Column(Integer, primary_key=True)
+    width = Column(String(250), nullable=False)
+    heigth = Column(String(250), nullable=False)
+    photo = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    profilephoto = relationship(User)
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    photo = Column(String(250), nullable=False)
+    video = Column(String(250), nullable=False)
+    text = Column(String(250), nullable=False)
+    tag = Column(String(250), nullable=False)
+    address = Column(String(250), nullable=False)
+    hashtag = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    post = relationship(User)
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    text = Column(String(250), nullable=False)
+    symbol = Column(String(250), nullable=True)
+    reference = Column(String(250), nullable=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('users.id'))
+    comment = relationship(Post)
     def to_dict(self):
         return {}
-
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
